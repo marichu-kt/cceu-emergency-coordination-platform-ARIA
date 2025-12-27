e9518c21538d99e74029d127c5df4e886e95ba51
marichu-kt <TU_EMAIL_DE_GITHUB@users.noreply.github.com>
2025-12-27 12:25:37 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/cceu-emergency-coordination/",
+  base: "/cceu-emergency-coordination-platform-ARIA/",
   plugins: [react()],
-  build: {
-    cssMinify: false,
-  },
 });
